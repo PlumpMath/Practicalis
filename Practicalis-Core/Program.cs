7af622d76ddcc6f193dc657bdebc017474018e08
Joshua Stroup <wargog@omacostudio.com>
2016-02-21 20:00:25 -0500

    Episode 2

--- Practicalis-Core/Program.cs
+++ Practicalis-Core/Program.cs
@@ -1,61 +1,33 @@
 ﻿using System;
-using System.Drawing;
-using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
-using OpenTK.Input;
+using Pencil.Gaming;
+using Pencil.Gaming.Graphics;
 
 namespace Practicalis
 {
     class Program
     {
-        [STAThread]
         static void Main(string[] args)
         {
-            using (var game = new GameWindow())
+            GlfwWindowPtr window = Window.createWindow();
+
+            while (!Glfw.WindowShouldClose(window))
             {
-                game.Load += (sender, e) =>
-                {
-                    //TODO: Load game settings, initialize textures and sounds
-                    game.VSync = VSyncMode.On;
-                };
+                Glfw.PollEvents();
 
-                game.Resize += (sender, e) =>
-                {
-                    GL.Viewport(0, 0, game.Width, game.Height);
-                };
+                if (Glfw.GetKey(window, Key.Escape))
+                    Glfw.SetWindowShouldClose(window, true);
 
-                game.UpdateFrame += (sender, e) =>
-                {
-                    //TODO: Handle input and game logic
-                    if (game.Keyboard[Key.Escape])
-                        game.Exit();
-                };
+                GL.ClearColor(Color4.Blue);
 
-                game.RenderFrame += (sender, e) =>
-                {
-                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-                    GL.MatrixMode(MatrixMode.Projection);
-                    GL.LoadIdentity();
-                    GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+                //TODO: Render stuff here
+                //render();
 
-                    GL.Begin(PrimitiveType.Triangles);
-
-                    GL.Color3(Color.MintCream);
-                    GL.Vertex2(-1.0f, 1.0f);
-                    GL.Color3(Color.Magenta);
-                    GL.Vertex2(0.0f, -1.0f);
-                    GL.Color3(Color.MidnightBlue);
-                    GL.Vertex2(1.0f, 1.0f);
-
-                    GL.End();
-
-                    game.SwapBuffers();
-                };
-
-                game.Run(60.0);
+                Glfw.SwapBuffers(window);
             }
+
+            Glfw.Terminate();
         }
     }
 }
---
+++ Practicalis-Core/Window.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Pencil.Gaming;
+using Pencil.Gaming.Graphics;
+
+namespace Practicalis
+{
+    class Window
+    {
+        private static readonly int WIDTH = 800;
+        private static readonly int HEIGHT = 600;
+        private static readonly string TITLE = "Practicalis";
+
+        static GlfwWindowPtr window;
+
+        public static GlfwWindowPtr createWindow()
+        {
+            if (!Glfw.Init())
+            {
+                Console.WriteLine("ERROR: Could not initialize GLFW");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            window = Glfw.CreateWindow(WIDTH, HEIGHT, TITLE, GlfwMonitorPtr.Null, GlfwWindowPtr.Null);
+
+            Glfw.MakeContextCurrent(window);
+
+            return window;
+        }
+    }
+}
