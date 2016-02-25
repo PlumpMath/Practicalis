bda1b8c47ce615bf9b541eb01fa7532fa500a68e
Joshua Stroup <wargog@omacostudio.com>
2016-02-24 20:24:26 -0500

    Updated to include broken support for arguments in CLI

--- Practicalis-CLI/Program.cs
+++ Practicalis-CLI/Program.cs
@@ -7,36 +7,58 @@ namespace Practicalis
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool runLoop = true;
+
+            string command;
+            string[] verbNoun;
+            string[] commandInfo;
+
+            if (args.Length > 0)
+            {
+                runLoop = false;
+
+                command = args[0];
+                verbNoun = command.Split('-');
+                commandInfo = verbNoun[1].Split(':');
+                
+                runCommand(verbNoun, commandInfo);
+            }
+
+            while (runLoop)
             {
                 Console.Write("Please enter a command: ");
-                string command = Console.ReadLine().ToString();
-                string[] verbnoun = command.Split('-');
-                string[] commandInfo = verbnoun[1].Split(' ');
+                command = Console.ReadLine().ToString();
+                verbNoun = command.Split('-');
+                commandInfo = verbNoun[1].Split(' ');
 
-                switch (verbnoun[0])
-                {
-                    case "Binarize":
-                        switch (commandInfo[0])
-                        {
-                            case "File":
-                                if (FileUtils.writeBinary(commandInfo[1]))
-                                    Console.WriteLine(commandInfo[1] + ".prbin written successfully");
-                                break;
-                            case "Directory":
-                                if (FileUtils.binarizeDirectory(commandInfo[1], false))
-                                    Console.WriteLine("Files in {0} binarized successfully!", commandInfo[1]);
-                                break;
-                            case "Resources":
-                                if (FileUtils.binarizeDirectory("Resources", true))
-                                    Console.WriteLine("Successfully binarized all Resource files");
-                                break;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Please enter a valid command and try again.");
-                        break;
-                }
+                runCommand(verbNoun, commandInfo);
+            }
+        }
+
+        private static void runCommand(string[] verbNoun, string[] commandInfo)
+        {
+            switch (verbNoun[0])
+            {
+                case "Binarize":
+                    switch (commandInfo[0])
+                    {
+                        case "File":
+                            if (FileUtils.writeBinary(commandInfo[1]))
+                                Console.WriteLine(commandInfo[1] + ".prbin written successfully");
+                            break;
+                        case "Directory":
+                            if (FileUtils.binarizeDirectory(commandInfo[1], false))
+                                Console.WriteLine("Files in {0} binarized successfully!", commandInfo[1]);
+                            break;
+                        case "Resources":
+                            if (FileUtils.binarizeDirectory("Resources", true))
+                                Console.WriteLine("Successfully binarized all Resource files");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid command and try again.");
+                    break;
             }
         }
     }
